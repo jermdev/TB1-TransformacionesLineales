a993975715e25996fca424032094f8e6f1186177
jermdev <jeremi.porras18@gmail.com>
2026-05-01 10:57:33 -0500

    Clase Abstracta Trasfomacion Implementada

--- Trasformacion.h
+++ Trasformacion.h
@@ -1,4 +1,19 @@
 #pragma once
+#include "Figura.h"
 class Trasformacion {
+private:
+	Figura* figura;
+public:
+
+	Trasformacion(Figura *figura) {
+		this->figura = figura;
+	}
+
+	~Trasformacion() {}
+
+	void setFigura(Figura* figura) { this->figura = figura; }
+	Figura* getFigura() { return this->figura ; }
+
+	virtual void trasformacion() = 0;
 };
 
